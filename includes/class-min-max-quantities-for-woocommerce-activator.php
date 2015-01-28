bf934a7572e2eaba0b150534a027854d053a8acc
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 17:23:14 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/min-max-quantities-for-woocommerce/trunk@1077672 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-min-max-quantities-for-woocommerce-activator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @class       MBJ_Min_Max_Quantities_For_WooCommerce_Activator
+ * @version	1.0.0
+ * @package	min-max-quantities-for-woocommerce
+ * @category	Class
+ * @author      johnny-manziel <jmkaila@gmail.com>
+ */
+class MBJ_Min_Max_Quantities_For_WooCommerce_Activator {
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate() {
+        
+    }
+
+}
