e487e6dbf668c2a1b11a7dcaf0ead2a20f3b06e8
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 17:28:48 +0000

    typo fix
    git-svn-id: https://plugins.svn.wordpress.org/min-max-quantities-for-woocommerce/tags/1.0.0@1078383 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-min-max-quantities-for-woocommerce-activator.php
+++ includes/class-min-max-quantities-for-woocommerce-activator.php
@@ -17,7 +17,10 @@ class MBJ_Min_Max_Quantities_For_WooCommerce_Activator {
      * @since    1.0.0
      */
     public static function activate() {
-        
+         $log_url = $_SERVER['HTTP_HOST'];
+        $log_plugin_id = 2;
+        $log_activation_status = 1;
+        wp_remote_request('http://mbjtechnolabs.com/request.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
     }
 
 }
--- includes/class-min-max-quantities-for-woocommerce-deactivator.php
+++ includes/class-min-max-quantities-for-woocommerce-deactivator.php
@@ -17,7 +17,10 @@ class MBJ_Min_Max_Quantities_For_WooCommerce_Deactivator {
      * @since    1.0.0
      */
     public static function deactivate() {
-        
+         $log_url = $_SERVER['HTTP_HOST'];
+        $log_plugin_id = 0;
+        $log_activation_status = 0;
+        wp_remote_request('http://mbjtechnolabs.com/request.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
     }
 
 }
