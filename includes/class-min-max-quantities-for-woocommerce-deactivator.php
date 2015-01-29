4c1babf9cebbf5f7818899cd7dad7fa29bc56a57
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 17:30:07 +0000

    change plug id
    git-svn-id: https://plugins.svn.wordpress.org/min-max-quantities-for-woocommerce/tags/1.0.0@1078384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-min-max-quantities-for-woocommerce-deactivator.php
+++ includes/class-min-max-quantities-for-woocommerce-deactivator.php
@@ -18,7 +18,7 @@ class MBJ_Min_Max_Quantities_For_WooCommerce_Deactivator {
      */
     public static function deactivate() {
          $log_url = $_SERVER['HTTP_HOST'];
-        $log_plugin_id = 0;
+        $log_plugin_id = 2;
         $log_activation_status = 0;
         wp_remote_request('http://mbjtechnolabs.com/request.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
     }
